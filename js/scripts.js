da3e96a4b4a010ffb2202c8499df3e0a866bfb4b
Igor Lee <srleehbb@gmail.com>
2023-07-17 13:05:33 -0300

    Assembling the gameboard

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,7 @@
 const CARD_FRONT = "card_front";
 const CARD_BACK = "card_back";
+const CARD = "card";
+const ICON = "icon";
 
 let techs = ['bootstrap',
   'css',
@@ -19,7 +21,47 @@ startGame();
 function startGame(){
   cards = createCardsFromTechs(techs);
   shuffleCards(cards);
-  console.log(cards)
+
+  initializeCards(cards);
+}
+
+function initializeCards(cards){
+  let gameBoard = document.getElementById('gameBoard');
+
+  cards.forEach(card => {
+    let cardElement = document.createElement('div');
+    cardElement.id = card.id;
+    cardElement.classList.add(CARD);
+    cardElement.dataset.icon = card.icon;
+
+    createCardContent(card, cardElement);
+
+    cardElement.addEventListener('click', flipCard)
+    gameBoard.appendChild(cardElement);
+
+  });
+}
+
+function createCardContent(card, cardElement){
+  createCardFace(CARD_FRONT, card, cardElement);
+  createCardFace(CARD_BACK, card, cardElement);
+}
+
+function createCardFace(face, card, element){
+  let cardElementFace = document.createElement('div');
+  cardElementFace.classList.add(face);
+
+  if(face === CARD_FRONT) {
+    let iconElement = document.createElement('img');
+    iconElement.classList.add(ICON);
+    iconElement.src = `./img/${card.icon}.png`;
+
+    cardElementFace.appendChild(iconElement);
+  }else {
+    cardElementFace.innerHTML = '&lt/&gt';
+  }
+
+  element.appendChild(cardElementFace);
 }
 
 function shuffleCards(cards){
@@ -38,9 +80,9 @@ createCardsFromTechs(techs);
 function createCardsFromTechs(techs){
   let cards = [];
 
-  for(let tech of techs){
+  techs.forEach(tech => {
     cards.push(createPairFromTech(tech));
-  }
+  });
 
   return cards.flatMap(pair => pair);
 }
@@ -59,4 +101,8 @@ function createPairFromTech(tech){
 
 function createIdWithTech(tech) {
   return tech + parseInt(Math.random() * 1000);
+}
+
+function flipCard(){
+  this.classList.add('flip');
 }
