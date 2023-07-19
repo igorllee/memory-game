fe4c37f25ef126438f637950fd9cd64d5f87cad4
Igor Lee <srleehbb@gmail.com>
2023-07-19 19:34:54 -0300

    Checking if exists pairs

--- js/game.js
+++ js/game.js
@@ -1,4 +1,37 @@
 let game = {
+  lockMode: false,
+  firstCard: null,
+  secondCard: null,
+
+  setCard: function(id){
+    let card = this.cards.filter(card=>card.id === id)[0];
+
+    if(card.flipped || this.lockMode){
+      return false;
+    }
+
+    if(!this.firstCard){
+      this.firstCard = card;
+      return true;
+    }else {
+      this.secondCard = card;
+      this.lockMode = true;
+      return true;
+    }
+  },
+
+  checkMatch: function(){
+    if(this.firstCard.icon === this.secondCard.icon){
+      return true;
+    }
+  },
+
+  clearCards: function(){
+    this.firstCard = null;
+    this.secondCard = null;
+    this.lockMode = false;
+  },
+
   techs: ['bootstrap',
     'css',
     'electron',
