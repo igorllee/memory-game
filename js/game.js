71753dbba8b48462bc887520fc4dbef501eeea2a
Igor Lee <srleehbb@gmail.com>
2023-07-19 19:46:18 -0300

    Fixed some bugs

--- js/game.js
+++ js/game.js
@@ -5,31 +5,40 @@ let game = {
 
   setCard: function(id){
     let card = this.cards.filter(card=>card.id === id)[0];
-
+    console.log(card);
     if(card.flipped || this.lockMode){
       return false;
     }
 
     if(!this.firstCard){
       this.firstCard = card;
+      this.firstCard.flipped = true;
       return true;
     }else {
       this.secondCard = card;
+      this.secondCard.flipped = true;
       this.lockMode = true;
       return true;
     }
   },
 
   checkMatch: function(){
-    if(this.firstCard.icon === this.secondCard.icon){
-      return true;
+    if(!this.firstCard || !this.secondCard){
+      return false;
     }
+    return this.firstCard.icon === this.secondCard.icon;
   },
 
   clearCards: function(){
     this.firstCard = null;
     this.secondCard = null;
     this.lockMode = false;
+  },
+
+  unflipCards: function() {
+    this.firstCard.flipped = false;
+    this.secondCard.flipped = false;
+    this.clearCards();
   },
 
   techs: ['bootstrap',
